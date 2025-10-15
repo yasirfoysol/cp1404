f0bf1996a9c7ada871cd6e3c4eaa4f65ed345dd9
yasirfoysol <yasirfoysol.rahib@my.jcu.edu.au>
2025-10-15 23:19:05 +1000

    Add number analysis and username access checker exercises

---
+++ week4/list_exercises.py
@@ -0,0 +1,28 @@
+"""
+CP1404 | Practical 04 | List Exercises
+"""
+def main():
+    numbers = []
+    for i in range(5):
+        number = int(input("Number: "))
+        numbers.append(number)
+
+    print(f"The first number is {numbers[0]}")
+    print(f"The last number is {numbers[-1]}")
+    print(f"The smallest number is {min(numbers)}")
+    print(f"The largest number is {max(numbers)}")
+    print(f"The average of the numbers is {sum(numbers) / len(numbers):.1f}")
+
+    # Woefully inadequate security checker
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45',
+                 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState',
+                 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+    username = input("Enter your username: ")
+    if username in usernames:
+        print("Access granted")
+    else:
+        print("Access denied")
+
+
+if __name__ == "__main__":
+    main()
